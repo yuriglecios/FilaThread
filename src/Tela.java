e80d3ec79fe7edc3357d5dadca75c4d5dec77364
Yuri <glleciosyuri@gmail.com>
2023-10-21 17:47:10 -0300

    Adicionando o label nome

--- src/Tela.java
+++ src/Tela.java
@@ -3,7 +3,10 @@ import java.awt.*;
 
 public class Tela extends JDialog {
 
-    private JPanel Painel = new JPanel();
+    private JPanel Painel = new JPanel(new GridBagLayout());
+
+    private JLabel campoNome = new JLabel("Nome:");
+    private JLabel campoEmail = new JLabel("Email:");
 
     public Tela(){
         // tudo o que for executado será dentro desse construtor
@@ -11,10 +14,15 @@ public class Tela extends JDialog {
         setSize(new Dimension(240,240));
         setLocationRelativeTo(null); // centraliza a tela e como não tem nemhum outro componente será null
         setResizable(false);
+
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        campoNome.setPreferredSize(new Dimension(200, 50));
+        Painel.add(campoNome);
 
+
+        add(Painel,BorderLayout.WEST);
         setVisible(true);
     }
 }
